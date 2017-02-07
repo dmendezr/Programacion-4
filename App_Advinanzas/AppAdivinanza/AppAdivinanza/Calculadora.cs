39b58ec313cf4b1dc86429ec45ac82d83734a512
Diego Mendez Rodriguez <dmendezrod.12@gmail.com>
2017-02-06 19:12:32 -0600

    Clase Calculadora y Frame Calculadora
    Clases de ejemplo de utilizacion de delegados.

---
+++ App_Advinanzas/AppAdivinanza/AppAdivinanza/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Ejercisio ejemplo de delegados*/
+
+namespace AppAdivinanza
+{
+    public class Calculadora
+    {
+        public static int Sumar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+    }
+}
---
+++ App_Advinanzas/AppAdivinanza/AppAdivinanza/CalculadoraFrame.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* Ejercisio ejemplo de delegados*/
+
+namespace AppAdivinanza
+{
+    public partial class CalculadoraFrame : Form
+    {
+
+        delegate int sumar(int n1, int n2);
+        sumar delSumar;
+
+        public CalculadoraFrame()
+        {
+            InitializeComponent();
+           delSumar = Calculadora.Sumar;
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+
+            int p1 = Convert.ToInt32(txtNum1.Text);
+            int p2 = Convert.ToInt32(txtNum2.Text);
+            txtNum3.Text = delSumar(p1, p2).ToString();
+        }
+    }
+}
