67b3558140807799bd23c27e51044a7a6d8d7e27
Diego Mendez Rodriguez <dmendezrod.12@gmail.com>
2017-02-20 20:38:35 -0600

    ComentariosBarco

--- Guerra_de_Barcos/Guerra_de_Barcos/Form1.cs
+++ Guerra_de_Barcos/Guerra_de_Barcos/Form1.cs
@@ -41,8 +41,8 @@ namespace Guerra_de_Barcos
             for (int i = 1; i < 2; i++)
             {
                 Random rdn1 = new Random();
-                int numeroAleatorio = rdn1.Next(0, 1);
-                listaPosicionEnemigo[Convert.ToInt32(numeroAleatorio)].contieneBarco = true;
+                int numeroAleatorio = rdn1.Next(0, 1);//Aleatorio para el indice del arreglo
+                listaPosicionEnemigo[Convert.ToInt32(numeroAleatorio)].contieneBarco = true; //Setear aleatoriamente segun el numero de indice
             }
         }
 
